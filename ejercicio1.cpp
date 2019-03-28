48d58c0e4728c366fea8cfa462c48787693f8925
bryan <bryan.diaz@utec.edu.pe>
2019-03-28 12:15:47 -0500

    first commit

---
+++ ejercicio1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	std::vector<char> letter;
+	string word;
+	int count = 0;
+	cout << "Ingrese una palabra: ";
+	for (int i = 0; i < word.length(); ++i)
+	{
+		if()
+		count++
+	}
+
+
+	return 0;
+}
