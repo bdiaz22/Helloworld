88b3d3796e6ee5ce23b5fe24ffc2149d19d0565a
bryan <bryan.diaz@utec.edu.pe>
2019-03-28 12:23:23 -0500

    vocales

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-
-int main()
-{
-	std::vector<char> letter;
-	string word;
-	int count = 0;
-	cout << "Ingrese una palabra: ";
-	for (int i = 0; i < word.length(); ++i)
-	{
-		if()
-		count++
-	}
-
-
-	return 0;
+ 
+void leeroracion(char& letra){
+ 
+    do{
+    cout << "Ilabra: " << endl;
+    cin.get(letra);
+    }while (letra!='.');
+}
+ 
+ 
+void contar(unsigned n){
+    char letra;
+    while (letra!='.'){
+    unsigned cont=0;
+        while (letra!=' '){
+            cont++;
+            cin.get(letra);
+        }
+        cout << cont << " ";
+    }
+}
+ 
+int main(){
+    unsigned n;
+    char letra;
+ 
+leeroracion (letra);
+ 
+contar (n);
+ 
+return 0;
 }
